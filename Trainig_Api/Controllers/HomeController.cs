36437e6043c4395ced2447cffec427c8efe730ad
Aleksandr Nikitin <Aleksandr_Nikitin3@epam.com>
2021-07-16 18:37:26 +0300

    перенос файлов в отдельную папку

--- Trainig_Api/Controllers/HomeController.cs
+++ Trainig_Api/Controllers/HomeController.cs
@@ -1,37 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using TrainingApi.Models;
 
 namespace TrainingApi.Controllers
 {
+    /// <summary>
+    /// HomeController
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        /// <summary>
+        /// Creates an instance of HomeController and saves the standard logger
+        /// </summary>
+        /// <param name="logger">Standard logger</param>
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        #region Methods
+        /// <summary>
+        /// Creates an Index view
+        /// </summary>
+        /// <returns>The result of an Action Method</returns>
         public IActionResult Index()
         {
             return View();
         }
-
+        /// <summary>
+        /// Creates a privacy view
+        /// </summary>
+        /// <returns>The result of an Action Method</returns>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Creates an Error view.
+        /// <para>Turns off cache while executing</para>
+        /// </summary>
+        /// <returns>The result of an Action Methods</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
     }
 }
--- Trainig_Api/Models/ErrorViewModel.cs
+++ Trainig_Api/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace TrainingApi.Models
 {
     public class ErrorViewModel
