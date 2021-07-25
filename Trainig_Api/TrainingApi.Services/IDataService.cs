76d987b9b61e78b7fa3c00fc4b27053c66e54146
Aleksandr Nikitin <Aleksandr_Nikitin3@epam.com>
2021-07-25 13:44:15 +0300

    finished with csv/sql data access in web

--- Trainig_Api/TrainingApi.Services/IDataService.cs
+++ Trainig_Api/TrainingApi.Services/IDataService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 
 namespace TrainingApi.Services
@@ -7,7 +9,9 @@ namespace TrainingApi.Services
     {
         string ConnectionString { get; set; }
         List<TModel> Data { get; set; }
+        Func<DataRow, TModel> CreateInstance { get; set; }
 
+        void Configure(Func<DataRow, TModel> action);
         List<TModel> GetData();
         List<TModel> GetData(CultureInfo culture);
         void WriteData(List<TModel> data);
---
+++ Trainig_Api/TrainingApi.Services/SqlDataService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace TrainingApi.Services
+{
+    public class SqlDataService<TModel> : IDataService<TModel>
+    {
+        private readonly IConfiguration _configuration;
+
+        public string ConnectionString { get; set; }
+
+        public List<TModel> Data { get; set; } = new List<TModel>();
+
+        public Func<DataRow, TModel> CreateInstance { get; set; }
+
+        public SqlDataService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration["ConnectionStrings:Sql"];
+        }
+
+        public void Configure(Func<DataRow, TModel> action)
+        {
+            CreateInstance = action;
+        }
+
+        public List<TModel> GetData()
+        {
+            return GetData(CultureInfo.InvariantCulture);
+        }
+
+        public List<TModel> GetData(CultureInfo culture)
+        {
+            using (SqlConnection sq = new(ConnectionString))
+            using (SqlDataAdapter adapter = new("SELECT * FROM dbo.Employee", sq))
+            {
+                DataTable recipeTable = new();
+                adapter.Fill(recipeTable);
+                foreach (DataRow row in recipeTable.Rows)
+                {
+                    if (CreateInstance != null) Data.Add(CreateInstance.Invoke(row));
+                }
+                return Data;
+            }
+        }
+
+        public void WriteData(List<TModel> data) => WriteData(data, CultureInfo.InvariantCulture);
+
+        public void WriteData(List<TModel> data, CultureInfo culture)
+        {
+
+        }
+
+    }
+}
