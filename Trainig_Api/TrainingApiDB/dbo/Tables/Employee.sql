30fee60d458bb0656f3a4659150af8d869c988c4
Aleksandr Nikitin <Aleksandr_Nikitin3@epam.com>
2021-07-25 17:09:52 +0300

    removed db project

--- Trainig_Api/TrainingApiDB/dbo/Tables/Employee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [EmployeeId] INT NOT NULL, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Age] INT NOT NULL, 
-    [EmailAdress] NVARCHAR(50) NOT NULL 
-)
