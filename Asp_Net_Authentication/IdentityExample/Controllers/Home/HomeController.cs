21c2714771cc5b7cfa49f7ba2992ecf84adfc7b1
niquitos <niquitos@yandex.ru>
2021-09-29 11:47:16 +0300

    IdentityExample - email verification

--- Asp_Net_Authentication/IdentityExample/Controllers/Home/HomeController.cs
+++ Asp_Net_Authentication/IdentityExample/Controllers/Home/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 using System.Threading.Tasks;
 
 namespace IdentityExample.Controllers.Home
@@ -9,11 +10,14 @@ namespace IdentityExample.Controllers.Home
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailService _emailService;
 
-        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager,
+            IEmailService emailService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailService = emailService;
         }
 
         public IActionResult Index()
@@ -68,16 +72,32 @@ namespace IdentityExample.Controllers.Home
 
             if (result.Succeeded)
             {
-                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-                if (signInResult.Succeeded)
-                {
-                    return RedirectToAction("Secret");
-                }
+                var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code }, Request.Scheme, Request.Host.ToString());
+
+                await _emailService.SendAsync("niquitos@yandex.ru", "Email verification", $"<a href=\"{link}\">Click here to verify your email.</a>", true);
+
+                return RedirectToAction("EmailVerification");
             }
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null) return BadRequest();
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+
+            if (result.Succeeded)
+                return View();
+            else return BadRequest();
+        }
+
+        public IActionResult EmailVerification() => View();
 
         public async Task<IActionResult> LogOut()
         {
